43934466811548a5f697dccaab1f887640352326
kamanaft <kamanaft@pm.me>
2021-06-12 21:23:13 +0200

    Hash function addad

--- compare.py
+++ compare.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
+import hashlib
 
+# Interface
 layout = [
     [sg.Text('File 1'), sg.InputText(), sg.FileBrowse(),
      sg.Checkbox('MD5'), sg.Checkbox('SHA1')
@@ -14,7 +16,24 @@ layout = [
 window = sg.Window('File Compare', layout)
 while True:
     event, values = window.read()
-    if event in (None, 'Exit', 'Cancel'):
-        break
+    if event not in (None, 'Exit', 'Cancel'):
+        continue
+    break
+
+# Function to work with hash
+def hash(file, alg):
+    if alg == 'MD5':
+        hash = hashlib.md5()
+    elif alg == 'SHA1':
+        hash = hashlib.sha1()
+    elif alg == 'SHA256':
+        hash = hashlib.sha256()
+    with open(file) as set:
+        for str in set:
+            hash.update(str.encode(encoding = 'utf-8'))
+    return(hash.hexdigest())
+
+
+
 
 
