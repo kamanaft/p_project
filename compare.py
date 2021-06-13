95a75daeff30bd4bcf6fb187d8f460f8f228d207
kamanaft <kamanaft@pm.me>
2021-06-13 19:39:31 +0200

    Logics added

--- compare.py
+++ compare.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import hashlib
+import re
 
 # Interface
 layout = [
@@ -13,13 +14,6 @@ layout = [
     [sg.Submit(), sg.Cancel()]
 ]
 
-window = sg.Window('File Compare', layout)
-while True:
-    event, values = window.read()
-    if event not in (None, 'Exit', 'Cancel'):
-        continue
-    break
-
 # Function to work with hashfiles
 def hash(file, alg):
     if alg == 'MD5':
@@ -30,10 +24,49 @@ def hash(file, alg):
         hash = hashlib.sha256()
     with open(file) as set:
         for str in set:
-            hash.update(str.encode(encoding = 'utf-8'))
-    return(hash.hexdigest())
+            hash.update(str.encode(encoding='utf-8'))
+    return hash.hexdigest()
 
+window = sg.Window('File Compare', layout)
 
-
+while True:
+    event, values = window.read()
+    if event in (None, 'Exit', 'Cancel'):
+        break
+    if event == 'Submit':
+        file1 = file2 = flag = None
+        if values[0] and values[3]:
+            file1 = re.findall('.', values[0])
+            file2 = re.findall('.', values[3])
+            flag = 1
+            if not file1 and file1 is not None:
+                print('Error: File 1 path is not valid.')
+                flag = 0
+            elif not file2 and file2 is not None:
+                print('Error: File 2 path is not valid.')
+                flag = 0
+            elif values[1] is not True and values[2] is not True and values[4] is not True:
+                print('Error: Choose at least one encryption algorithm')
+            elif flag == 1:
+                print('Info: Filepaths were defined.')
+                algs = []
+                if values[1] == True: algs.append('MD5')
+                if values[2] == True: algs.append('SHA1')
+                if values[4] == True: algs.append('SHA256')
+                filepaths = []
+                filepaths.append(values[0])
+                filepaths.append(values[3])
+                print('Info: File comparison using:', algs)
+                for alg in algs:
+                    print(alg, ':')
+                    print(filepaths[0], ':', hash(filepaths[0], alg))
+                    print(filepaths[1], ':', hash(filepaths[1], alg))
+                    if hash(filepaths[0], alg) == hash(filepaths[1], alg):
+                        print('Files match for ', alg)
+                    else:
+                        print('Files do NOT match for ', alg)
+        else:
+            print('Please choose 2 files.')
+window.close()
 
 
